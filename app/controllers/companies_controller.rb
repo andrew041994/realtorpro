752ce805b98d5e80aac99e44e18a7e2c39859d6b
andrew041994 <andrew041994@gmail.com>
2020-04-14 18:20:27 -0400

    edited agent migration

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,29 @@
+class CompaniesController < ApplicationController
+    def home
+    end
+
+    def new
+        @company = Company.new        
+    end
+
+    def create
+     @company = Company.new(company_params)
+
+     @company.save
+      redirect_to @company        
+    end
+
+    def show
+        @company = Company.find(params[:id])
+        
+    end
+
+
+
+    private
+
+    def company_params
+        params.require(:company).permit( :name)
+        
+    end
+end
--- db/migrate/20200414221827_create_agents.rb
+++ db/migrate/20200414221827_create_agents.rb
@@ -1,8 +1,11 @@
 class CreateAgents < ActiveRecord::Migration[6.0]
   def change
     create_table :agents do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
+      t.string :username
       t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
